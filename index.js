670dcdf6d06fa88f962c9f93aa548f42e8f60327
Vinod Chauhan <vinod@mediccreations.com>
2020-04-29 16:56:45 +0530

    Reactotron Configure
    Reactotron successfully configured for both android and ios in react native and for connecting reactron to android run adb reverse tcp:9090 tcp:9090 command and refresh android app and for ios just run the app in simulator or device and for any configuration change please see the reactotron-config file

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// importing reactotron only in dev environment
+if (__DEV__) {
+    import('./reactotron-config').then(() => console.log('Reactotron Configured'));
+}
+
 import { AppRegistry } from 'react-native'
 import App from './app/App'
 import { name as appName } from './app.json'
---
+++ reactotron-config.js
@@ -0,0 +1,16 @@
+import Reactotron from 'reactotron-react-native'
+
+Reactotron
+    .configure({
+        name: "HandWashTimer" // name of the product
+    })
+    .useReactNative({
+        asyncStorage: false, // there are more options to the async storage.
+        networking: { // optionally, you can turn it off with false.
+            ignoreUrls: /symbolicate/
+        },
+        editor: false, // there are more options to editor
+        errors: { veto: () => false }, // or turn it off with false
+        overlay: false, // just turning off overlay
+    })
+    .connect(); // connect to the reactotron
