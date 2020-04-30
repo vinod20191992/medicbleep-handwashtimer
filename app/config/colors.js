2bfe07031e1106e192855ec7519f9671156859df
Bhuvneshwar Garg <bhuvneshwar@mediccreations.com>
2020-04-30 16:23:19 +0530

    Colors added

--- app/config/colors.js
+++ app/config/colors.js
@@ -2,4 +2,9 @@ export default {
   blue: '#0000FF',
   black: '#000000',
   green: '#00A778',
+  navigationHeaderColor: '#18a679',
+  white: '#FFFFFF',
+  dayTextColor: '#4A4A4A',
+  dateTextColor: '#9b9b9b',
+  separatorLineGrayColor: '#E5E9EA',
 }
